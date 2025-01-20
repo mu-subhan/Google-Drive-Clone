e5e4d09b8c0b314d6ba7750ca13993422239bc78
Muhammad Subhan <muhammadsubhan8934@gmail.com>
2025-01-20 17:00:57 +0500

    dynamic routes are done

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -1,11 +1,12 @@
 'use server'
 
-import { ID } from "node-appwrite";
+import { ID, Models, Query } from "node-appwrite";
 import { createAdminClient } from "../appwrite";
 import {InputFile} from "node-appwrite/file"
 import { appwriteConfig } from "../appwrite/config";
 import { constructFileUrl, getFileType, parseStringify } from "../utils";
 import { revalidatePath } from "next/cache";
+import { getCurrentUser } from "./user.actions";
 
 const handleError = (error:unknown,message:string) =>{
     console.log(error,message);
@@ -50,4 +51,34 @@ try {
 } catch (error) {
     handleError(error,"Failed to upload file")
 }
+}
+
+const createQuries = (currentUser:Models.Document) =>{
+    const queries = [
+        Query.or([
+            Query.equal("owner",[currentUser.$id]),
+            Query.contains("users",[currentUser.email]),
+        ]),
+    ]
+    return queries;
+}
+
+export const getFiles = async() => {
+    const {databases} = await createAdminClient();
+
+    try {
+        const currentUser = await getCurrentUser();
+        if(!currentUser) throw new Error("User not founf");
+
+        const queries = createQuries(currentUser);
+
+        const files = await databases.listDocuments(
+            appwriteConfig.databaseId,
+            appwriteConfig.filesCollectionId,
+            queries,
+        )
+        return parseStringify(files);
+    } catch (error) {
+        handleError(error,"Failed to get file")
+    }
 }
