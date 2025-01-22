91ea9a036c9bb099dbbd5dd31772df1fc45ec2fd
Muhammad Subhan <muhammadsubhan8934@gmail.com>
2025-01-22 15:19:31 +0500

    updating layout.tsx file

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -7,6 +7,8 @@ import { getCurrentUser } from '@/lib/actions/user.actions'
 import { redirect } from 'next/navigation'
 import React from 'react'
 
+export const dynamic = "force-dynamic"
+
 const Layout = async ({ children }: { children: React.ReactNode }) => {
     const currentUser = await getCurrentUser();
 console.log(currentUser,"curent user");
